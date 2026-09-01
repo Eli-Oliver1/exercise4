add0e0ffba8b1cb610a9a6d2bc226497566225d6
Eli-Oliver1 <epolivernc@gmail.com>
2026-09-01 18:00:15 -0400

    Update drawstuff.js

--- drawstuff.js
+++ drawstuff.js
@@ -523,10 +523,10 @@ function main() {
     };
 
     var poly = [
-        {x:-5,y:5,z:10,c:new Color(255,0,0,255)},
-        {x:5,y:5,z:10,c:new Color(0,255,0,255)},
-        {x:5,y:-5,z:10,c:new Color(0,0,0,255)},
-        {x:-5,y:-5,z:10,c:new Color(0,0,255,255)}
+        {x:-5,y:5,z:10,c:new Color(0,255,0,255)},
+        {x:5,y:5,z:10,c:new Color(255,0,0,255)},
+        {x:5,y:-5,z:10,c:new Color(0,0,255,255)},
+        {x:-5,y:-5,z:10,c:new Color(0,0,0,255)}
     ];
 
     // project and fill
